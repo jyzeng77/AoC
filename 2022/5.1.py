855557e69e609fd19eda4422255bb5c83cb7c524
jyzeng77 <jerryzeng59@gmail.com>
2022-12-09 16:39:12 -0500

    [yesterday] Completed Day 6, incomplete Day 5

--- 2022/5.1.py
+++ 2022/5.1.py
@@ -1,13 +1,46 @@
 import re 
 
+stacks = []
+
 # Flag: Collecting the items in stacks? 
 # Or can we continue to the rearrangement?
 taking_stock = True   
 
+# Do we need to count the number of stacks?
+counting = True
+
 with open('ex5.txt', 'r') as f:
-    num_columns = int(len(f.readline()) / 4)
-    stacks = [[] for i in range(num_columns)]
 
     for line in f:
-        print(line)
+        if counting:
+            num_columns = int(len(line) / 4)
+            stacks = [[]]*num_columns
+            counting = False
 
+        if taking_stock:
+            processed_line = list(line[1:-1:4])
+            print(processed_line)
+            if '1' in processed_line:
+                taking_stock = False
+                continue
+            else:
+                for i in range(len(stacks)):
+                    if not processed_line[i].isspace():
+                        print(i)
+                        stacks[i].insert(0, processed_line[i])
+                    print(stacks)
+                    
+        if not taking_stock:
+            print('moving')
+            if line.strip() == '':
+                continue
+            number, move_from, move_to= re.findall(r'\d+', line)
+            number, move_from, move_to = int(number), int(move_from)-1, int(move_to)-1
+
+
+            stacks[move_to].extend(stacks[move_from][-number:len(stacks[move_from])])
+            del stacks[move_from][-number:len(stacks[move_from])]
+
+for stack in stacks:
+    print(stack[-1],end='')
+print()
---
+++ 2022/6.1.py
@@ -0,0 +1,20 @@
+# stream = list(input('Stream: '))
+stream = list(open('p6.txt').read())
+print(stream)
+
+def duplicates_present(x):
+    checked = []
+    for elem in x:
+        if elem in checked:
+            return True
+        else:
+            checked.append(elem)
+    return False
+
+for i in range(len(stream)):
+    marker = stream[i-4:i]
+
+    if len(marker) != 0:
+        if not duplicates_present(marker):
+            print(i)
+            break
